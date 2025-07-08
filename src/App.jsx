42f4225e59c677c31dde46783e642169e33907a8
Eduardo <emejia0@yahoo.com>
2025-07-08 15:28:33 -0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,29 @@
+import Background from "./components/Background/Background";
+import Navbar from "./components/Navbar/Navbar";
+import { useState } from "react";
+import Hero from "./components/Hero/Hero";
+
+const App = () => {
+  let heroData = [
+    { text1: "Dive into", text2: "what you love" },
+    { text1: "Indulge", text2: "your passion" },
+    { text1: "Give in to", text2: "your passions" },
+  ];
+  const [heroCount, setHeroCount] = useState(2);
+  const [playStatus, setPlayStatus] = useState(false);
+  return (
+    <div>
+      <Background playStatus={playStatus} heroCount={heroCount} />
+      <Navbar />
+      <Hero
+        setPlayStatus={setPlayStatus}
+        heroData={heroData[heroCount]}
+        heroCount={heroCount}
+        setHeroCount={setHeroCount}
+        playStatus={playStatus}
+      />
+    </div>
+  );
+};
+
+export default App;
